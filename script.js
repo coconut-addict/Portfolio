6d423bb582fba85d6317228e46326fc23b279995
diana <dianaaladzic@gmail.com>
2024-07-25 15:17:20 +0100

    Media Queries

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 // Sticky Header
 
 const header = document.querySelector('header');
+const menuIcon = document.getElementById('menu-icon');
+const navlist = document.querySelector('.navlist');
 
 window.addEventListener ('scroll', function(){
     
     header.classList.toggle ('sticky', window.scrollY > 0);
 })
-
+// Toggle navigation menu on small screens
+menuIcon.addEventListener('click', function() {
+    navlist.classList.toggle('active');
+});
 
 // MouseOver MouseOut on skills icons
 
